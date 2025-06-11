65aac5e69caf5f03716668164d31071e76627150
GormlessOstrich <gormlessostrich@gmail.com>
2025-06-11 08:59:39 +0100

    finished project

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+import sys
 from stats import word_count
 from stats import char_count
+from stats import dict_sort
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = sys.argv[1]
+    text = get_book_text(book)
     words = word_count(text)
     characters = char_count(text)
-    #L3: print(get_book_text("books/frankenstein.txt"))
+    sorted_list = dict_sort(characters)
+    #L3: print(get_book_text(book))
     #L4: print(f"{word_count(text)} words found in the document")
-    print(words)
-    print(characters)
+    #print(words)
+    #print(characters)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
 
 def get_book_text(relative_path):
     file_contents = ""
--- stats.py
+++ stats.py
@@ -11,3 +11,18 @@ def char_count(text):
         else:
             count[char] = 1
     return count
+
+def get_values(char_dict):
+    return char_dict["num"]
+
+def dict_sort(char_dict):
+    new_list = []
+    for key, value in char_dict.items():
+        new_dict = {
+            "char": key,
+            "num": value
+            }
+        if key.isalpha():
+            new_list.append(new_dict)
+    new_list.sort(reverse=True, key=get_values)
+    return new_list
